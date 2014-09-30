58e8793b06418187133d35ac2196b0b74d3616cf
Dan Williams <dan@osheim.org>
2014-09-29 20:53:51 -0500

    added pyrento to notebook

--- ipython/ipython_notebook_config.py
+++ ipython/ipython_notebook_config.py
@@ -1,6 +1,23 @@
 # Dan's configuration file for ipython-notebook.
 
+import imp
+
 c = get_config()
+
+def add_import(name, result_lst):
+    try:
+        imp.find_module(name)
+        c = get_config()
+        c.IPKernelApp.exec_lines.append('import {}'.format(name))
+        result_lst.append('  [yes {}]'.format(name))
+    except ImportError, e:
+        result_lst.append('  [no  {}]'.format(name))
+        return False
+    return True
+
+
+c.IPKernelApp.exec_lines = []
+add_import('pyrento', [])
 
 # The IP address the notebook server will listen on.
 c.NotebookApp.ip = '0.0.0.0'
@@ -13,3 +30,4 @@ if os.environ.has_key('IPY_NOTES'):
     c.NotebookManager.notebook_dir = os.environ['IPY_NOTES']
 else:
     c.NotebookManager.notebook_dir = os.environ['HOME']
+
